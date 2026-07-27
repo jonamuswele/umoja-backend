c16a65f5b3757e53aac50e300f70bac671d68fa2
jonamuswele <jonathankaleme@gmail.com>
2026-07-27 14:15:01 +0100

    Remove unnecessary blank lines in entry.py

--- entry.py
+++ entry.py
@@ -1,8 +1,6 @@
 import asgi
 from workers import WorkerEntrypoint
 from main import app
-
-
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
         return await asgi.fetch(app, request.js_object, self.env)
