71301d7d039a3704a933ae4a636d6b9e680bba54
jonamuswele <jonathankaleme@gmail.com>
2026-07-27 14:05:21 +0100

    Update entry.py

--- entry.py
+++ entry.py
@@ -1,6 +1,18 @@
+import glob
+import os
+import sys
+
+# Ensure all site-packages directories under .venv are added to sys.path
+base_dir = os.path.dirname(os.path.abspath(__file__))
+for sp in glob.glob(
+    os.path.join(base_dir, ".venv", "**", "site-packages"), recursive=True
+):
+    if sp not in sys.path:
+        sys.path.insert(0, sp)
+
 import asgi
-from workers import WorkerEntrypoint
 from main import app
+from workers import WorkerEntrypoint
 
 
 class Default(WorkerEntrypoint):
