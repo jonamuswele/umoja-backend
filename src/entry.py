e7f225e028428304b8e61663ff300c2575f61d24
jonamuswele <jonathankaleme@gmail.com>
2026-07-27 09:29:52 +0000

    reorganizing

--- src/entry.py
+++ src/entry.py
@@ -2,27 +2,7 @@ import asgi
 from fastapi import FastAPI, Request
 from fastapi.middleware.cors import CORSMiddleware
 from workers import WorkerEntrypoint 
-
-app = FastAPI()
-
-#Enable CORS for frontend
-app.add_middleware(
-  CORSMiddleware,
-  allow_origins=["*"],
-  allow_credentials=True,
-  allow_methods=["*"],
-  allow_headers=["*"],
-)
-
-@app.get("api/health")
-async def health():
-  return {"status":"online"}
-
-@app.get("/api/users")
-async def get_users(req: Request):
-  env = req.scope["env"]
-  result = await env.DB.prepare("SELECT * FROM users").all()
-  return {"users": result.results}
+from main import app
 
 class Default(WorkerEntrypoint):
   async def fetch(self, request):
