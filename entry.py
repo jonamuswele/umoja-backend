63127a43cde7856c964a1cb3c62bb049e730234b
jonamuswele <jonathankaleme@gmail.com>
2026-07-27 13:21:19 +0100

    Update fetch method to use request.js_object

--- entry.py
+++ entry.py
@@ -5,4 +5,4 @@ from main import app
 
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        return await asgi.fetch(app, request, self.env)
+        return await asgi.fetch(app, request.js_object, self.env)
