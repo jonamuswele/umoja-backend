3c51e22c8f5eae206bc2d724631846ebe8f288a2
jonamuswele <jonathankaleme@gmail.com>
2026-07-27 11:40:40 +0100

    Refactor fetch method to import from workers.asgi

--- src/entry.py
+++ src/entry.py
@@ -1,9 +1,7 @@
-import asgi
-from fastapi import FastAPI, Request
-from fastapi.middleware.cors import CORSMiddleware
-from workers import WorkerEntrypoint 
+from workers import WorkerEntrypoint
 from main import app
 
 class Default(WorkerEntrypoint):
-  async def fetch(self, request):
-    return await asgi.fetch(app, request, self.env)
+    async def fetch(self, request):
+        from workers.asgi import fetch
+        return await fetch(app, request, self.env)
