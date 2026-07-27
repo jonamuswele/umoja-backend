326291611915d6b1ddcd6ce7b4abbaabb561ec6f
jonamuswele <jonathankaleme@gmail.com>
2026-07-27 12:07:52 +0100

    Refactor fetch import to use asgi module

--- src/entry.py
+++ src/entry.py
@@ -1,7 +1,8 @@
+import asgi
 from workers import WorkerEntrypoint
 from main import app
 
+
 class Default(WorkerEntrypoint):
     async def fetch(self, request):
-        from workers.asgi import fetch
-        return await fetch(app, request, self.env)
+        return await asgi.fetch(app, request, self.env)
